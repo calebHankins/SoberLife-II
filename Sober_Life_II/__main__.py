fbf45dad85b21b0bbca7d4c9d5966a6c7ac9644d
Caleb Hankins <chankins@gmail.com>
2019-08-09 17:44:45 -0500

    cleaning up main script

--- Sober_Life_II/__main__.py
+++ Sober_Life_II/__main__.py
@@ -1,10 +1,6 @@
 def main():
-
     print("This is the main routine.")
-    print("It should do something interesting.")
-    # print("todo:" + Sober_Life_II.)
 
 
 if __name__ == "__main__":
-    # execute only if run as a script
     main()
