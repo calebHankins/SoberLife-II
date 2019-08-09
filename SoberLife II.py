76b9b2df04e5fa6ed14ac275ab60751f13a189db
Caleb Hankins <chankins@gmail.com>
2019-08-09 15:25:49 -0500

    Modifying pyinstaller spec to attempt to produce a viable exe

--- SoberLife II.py
+++ SoberLife II.py
@@ -1,4 +1,12 @@
+import sys
+import os
 from Sober_Life_II.__main__ import main
+
+# if getattr(sys, 'frozen', False):
+#     os.chdir(os.path.dirname(sys.executable))
+
+if getattr(sys, 'frozen', False):
+    os.chdir(sys._MEIPASS)
 
 if __name__ == '__main__':
     main()
