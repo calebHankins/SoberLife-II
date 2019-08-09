62ccf18515fb99071ac502aead3ed276dab0d99c
Caleb Hankins <chankins@gmail.com>
2019-08-09 14:55:18 -0500

    Added License, pyinstaller entry point, and Manifest files

---
+++ SoberLife II.py
@@ -0,0 +1,4 @@
+from Sober_Life_II.__main__ import main
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='Sober_Life_II',
+    license='LGPL',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
@@ -21,13 +22,15 @@ setup(
     #     "icon_resources": [(1, "./assets/icon.ico")],
     #     "dest_base":"SoberLifeII"
     # }],
-    package_data={'': ['*.png']},
-    packages=find_packages('Sober_Life_II'),
+    # package_data={'': ['*.png']},
+    include_package_data=True,
+    package_dir={'Sober_Life_II': 'Sober_Life_II'},
+    packages=find_packages(),
     install_requires=requirements,
     version='0.0.1',
     entry_points={
-        # 'gui_scripts': [
-        'console_scripts': [
+        'gui_scripts': [
+        # 'console_scripts': [
             'SoberLifeII = Sober_Life_II.__main__:main',
         ],
     },
